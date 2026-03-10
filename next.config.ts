a128c2f967392083a4a54a63176dedd7c890e216
aliyakhansa21 <aliyakamaliya21@gmail.com>
2026-03-10 23:11:19 +0700

    fix(next.config): update image remote patterns for local development

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/storage/**',
+      },
+    ],
   },
 };
 
