af4a5ed1efe96323ed5701d5c50958477b9c13c7
Aliya Khansa Kamaliya <aliyakamaliya21@gmail.com>
2026-05-21 15:26:54 +0700

    fix(api): update baseURL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://resto-kita-production-1fa0.up.railway.app/api",
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api",
     headers: {
         "Content-Type": "application/json",
     },
